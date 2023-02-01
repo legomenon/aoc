58eb0002f810b51a5c4100b5a8b943589a83a757
legomenon <solomovsky.roman@gmail.com>
2023-02-01 15:36:36 +0200

    aoc 2021 add day 1 part 1 and 2

---
+++ 2021/day1/src/main.rs
@@ -0,0 +1,43 @@
+use std::cmp::Ordering;
+use std::fs;
+
+fn main() {
+    let file = fs::read_to_string("file.txt").unwrap();
+    let p1 = part1(&file);
+    dbg!(p1);
+    let p2 = part2(&file);
+    dbg!(p2);
+}
+
+fn part1(file: &str) -> u32 {
+    let (current, _other) = file.split_once('\n').unwrap();
+    let mut current = current.parse::<u32>().unwrap();
+    file.split('\n')
+        .map(|x| x.parse::<u32>().unwrap())
+        .map(|x| {
+            if let Ordering::Greater = x.cmp(&current) {
+                current = x;
+                return x;
+            } else {
+                current = x;
+                0
+            }
+        })
+        .filter(|x| *x != 0)
+        .count() as u32
+}
+
+fn part2(file: &str) -> u32 {
+    let res = file
+        .split('\n')
+        .map(|x| x.parse::<u32>().unwrap())
+        .collect::<Vec<_>>()
+        .windows(3)
+        .map(|x| x.iter().sum::<u32>())
+        .inspect(|x| println!("{:?}", x))
+        .map(|x| x.to_string())
+        .collect::<Vec<_>>()
+        .join("\n");
+
+    part1(&res)
+}
