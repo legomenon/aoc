74c61dbb2e49502de8330b36106d8500d7bd7ecb
legomenon <solomovsky.roman@gmail.com>
2023-02-04 20:36:39 +0200

    add aoc 2019 day par 2 and updated part 1

--- 2019/day2/src/main.rs
+++ 2019/day2/src/main.rs
@@ -1,5 +1,6 @@
 use std::fs;
 
+#[derive(Debug)]
 enum Opcode {
     Add = 1,
     Mul = 2,
@@ -9,37 +10,80 @@ enum Opcode {
 fn main() {
     let file = fs::read_to_string("file.txt").unwrap();
     let p1 = part1(&file);
-    dbg!(p1);
+    let p2 = part2(&file, 19690720);
+    dbg!(p1, p2);
 }
 
 fn part1(file: &str) -> usize {
-    let mut intcode_vec = file
-        .split(',')
-        .map(|x| x.parse::<usize>().unwrap())
-        .collect::<Vec<_>>();
+    let mut intcode_vec = parse(file);
+
+    intcode_vec[1] = 12;
+    intcode_vec[2] = 2;
 
     let i_iter = intcode_vec.clone();
-    let mut i_iter = i_iter.iter();
+    let mut i_iter = i_iter.into_iter();
 
     loop {
-        let opcode = Opcode::try_from(*i_iter.next().unwrap()).unwrap();
+        let opcode = Opcode::try_from(i_iter.next().unwrap()).expect("invalid opcode value");
         match opcode {
             Opcode::Add => {
                 let a = i_iter.next().unwrap();
                 let b = i_iter.next().unwrap();
                 let c = i_iter.next().unwrap();
-                intcode_vec[*c] = intcode_vec[*a] + intcode_vec[*b];
+                intcode_vec[c] = intcode_vec[a] + intcode_vec[b];
             }
             Opcode::Mul => {
                 let a = i_iter.next().unwrap();
                 let b = i_iter.next().unwrap();
                 let c = i_iter.next().unwrap();
-                intcode_vec[*c] = intcode_vec[*a] * intcode_vec[*b]
+                intcode_vec[c] = intcode_vec[a] * intcode_vec[b]
             }
             Opcode::Ext => break,
         }
     }
     intcode_vec[0]
+}
+
+fn part2(file: &str, num: usize) -> usize {
+    for i in 0..=99 {
+        for j in 0..=99 {
+            let mut intcode_vec = parse(&file);
+            let i_iter = intcode_vec.clone();
+            let mut i_iter = i_iter.into_iter();
+
+            intcode_vec[1] = i;
+            intcode_vec[2] = j;
+
+            loop {
+                let opcode = match Opcode::try_from(i_iter.next().unwrap()) {
+                    Ok(opcode) => opcode,
+                    Err(_) => continue,
+                };
+
+                match opcode {
+                    Opcode::Add => {
+                        let a = i_iter.next().unwrap();
+                        let b = i_iter.next().unwrap();
+                        let c = i_iter.next().unwrap();
+                        intcode_vec[c] = intcode_vec[a] + intcode_vec[b];
+                    }
+                    Opcode::Mul => {
+                        let a = i_iter.next().unwrap();
+                        let b = i_iter.next().unwrap();
+                        let c = i_iter.next().unwrap();
+                        intcode_vec[c] = intcode_vec[a] * intcode_vec[b]
+                    }
+                    Opcode::Ext => {
+                        if intcode_vec[0] == num {
+                            return 100 * intcode_vec[1] + intcode_vec[2];
+                        }
+                        break;
+                    }
+                }
+            }
+        }
+    }
+    0
 }
 
 impl TryFrom<usize> for Opcode {
@@ -50,7 +94,13 @@ impl TryFrom<usize> for Opcode {
             i if i == Opcode::Add as usize => Ok(Opcode::Add),
             i if i == Opcode::Mul as usize => Ok(Opcode::Mul),
             i if i == Opcode::Ext as usize => Ok(Opcode::Ext),
-            _ => Err("invalid opcode value".to_owned()),
+            _ => Err("ivalid opcode".to_owned()),
         }
     }
+}
+
+fn parse(file: &str) -> Vec<usize> {
+    file.split(',')
+        .map(|x| x.parse::<usize>().unwrap())
+        .collect::<Vec<_>>()
 }
