082ff3705e31e6c4105f47a9f43dbd1bcb42fe87
legomenon <solomovsky.roman@gmail.com>
2023-02-05 13:25:59 +0200

    add aoc 2020 day 3 part 2

--- 2020/day3/src/main.rs
+++ 2020/day3/src/main.rs
@@ -4,21 +4,51 @@ fn main() {
     let file = fs::read_to_string("file.txt").unwrap();
     let forest = parse(&file);
 
-    let p1 = part1(forest);
-    dbg!(p1);
+    let p1 = part1(&forest, 3);
+    let p2 = part2(&forest, 7);
+
+    dbg!(p1, p2);
 }
 
-fn part1(forest: Vec<Vec<char>>) -> usize {
+fn part1(forest: &Vec<Vec<char>>, step: usize) -> usize {
     let mut pos_x: usize = 0;
     forest
         .iter()
         .map(|tree_row| {
             let square = tree_row[pos_x];
-            pos_x = (pos_x + 3) % tree_row.len();
+            pos_x = (pos_x + step) % tree_row.len();
             square
         })
         .filter(|&c| c == '#')
         .count()
+}
+
+fn part2(forest: &Vec<Vec<char>>, max_step: usize) -> usize {
+    let mut result: Vec<usize> = Vec::new();
+
+    let steps = (1..=max_step)
+        .filter(|i| i % 2 != 0)
+        .collect::<Vec<usize>>();
+
+    for i in steps {
+        result.push(part1(forest, i))
+    }
+
+    let down_2 = forest
+        .iter()
+        .cloned()
+        .enumerate()
+        .filter_map(move |(i, row)| {
+            if i % 2 == 0 {
+                return Some(row);
+            }
+            None
+        })
+        .collect::<Vec<_>>();
+
+    result.push(part1(&down_2, 1));
+
+    result.iter().fold(1, |acc, x| acc * x)
 }
 
 fn parse(file: &str) -> Vec<Vec<char>> {
