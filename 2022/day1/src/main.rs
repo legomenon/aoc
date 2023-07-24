2bbf1126f062f5cadd9718d2ffa8cc693ff51b5c
legomenon <solomovsky.roman@gmail.com>
2023-07-24 11:43:54 +0300

    update aoc 2022 day 1: add error handling

--- 2022/day1/src/main.rs
+++ 2022/day1/src/main.rs
@@ -1,16 +1,22 @@
+use color_eyre::Result;
 use std::fs;
 
-fn main() {
-    let file = fs::read_to_string("file.txt").unwrap();
-    let mut calories = parse(&file);
+fn main() -> Result<()> {
+    color_eyre::install()?;
+    let mut calories = parse("file.txt")?;
 
-    let p1 = part1(&calories);
+    let p1 = part1(&calories)?;
     let p2 = part2(&mut calories);
     dbg!(p1, p2);
+
+    Ok(())
 }
 
-fn part1(v: &[u32]) -> u32 {
-    *v.iter().max().unwrap()
+fn part1(v: &[u32]) -> Result<u32> {
+    v.iter()
+        .max()
+        .cloned()
+        .ok_or_else(|| color_eyre::eyre::eyre!("Slice is empty"))
 }
 
 fn part2(v: &mut [u32]) -> u32 {
@@ -18,13 +24,16 @@ fn part2(v: &mut [u32]) -> u32 {
     v.iter().take(3).sum::<u32>()
 }
 
-fn parse(file: &str) -> Vec<u32> {
-    file.split("\n\n")
+fn parse(p: &str) -> Result<Vec<u32>> {
+    let file = fs::read_to_string(p)?;
+    let data = file
+        .split("\n\n")
         .map(|elf_chunks| {
             elf_chunks
-                .split('\n')
-                .map(|cal| cal.parse::<u32>().unwrap())
-                .sum::<u32>()
+                .lines()
+                .map(|cal| cal.parse::<u32>())
+                .sum::<Result<u32, _>>()
         })
-        .collect()
+        .collect::<Result<Vec<u32>, _>>()?;
+    Ok(data)
 }
