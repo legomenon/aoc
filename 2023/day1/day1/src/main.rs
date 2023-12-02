d5e5d46be06ef712d0118a8e446c214d26401bbb
legomenon <solomovsky.roman@gmail.com>
2023-12-02 12:09:13 +0200

    add aoc 2023 day1 part 1

--- 2023/day1/day1/src/main.rs
+++ 2023/day1/day1/src/main.rs
@@ -1,9 +1,10 @@
 use std::fs;
 
 fn main() {
-    let a = fs::read_to_string("file.txt").unwrap();
-    let p1 = part1(&a);
-    dbg!(p1);
+    let data = fs::read_to_string("file.txt").unwrap();
+    let p1 = part1(&data);
+    let p2 = part2(&data);
+    dbg!(p1, p2);
 }
 
 fn part1(file: &str) -> u32 {
@@ -14,4 +15,35 @@ fn part1(file: &str) -> u32 {
             num.parse::<u32>().unwrap_or_default()
         })
         .sum()
+}
+
+fn part2(file: &str) -> u32 {
+    let v = file.lines().map(|l| decode(l)).collect::<String>();
+    let res = part1(&v);
+    dbg!(res);
+    res
+}
+
+fn decode(line: &str) -> String {
+    let mut l = line.to_owned();
+    l.push('\n');
+    let nums: Vec<(&str, &str)> = vec![
+        ("zero", "z0o"),
+        ("one", "o1e"),
+        ("two", "t2o"),
+        ("three", "t3e"),
+        ("four", "f4r"),
+        ("five", "f5e"),
+        ("six", "s6x"),
+        ("seven", "s7n"),
+        ("eight", "e8t"),
+        ("nine", "n9e"),
+    ];
+
+    for (num, rep) in nums {
+        if l.contains(num) {
+            l = l.replace(num, rep);
+        }
+    }
+    l
 }
